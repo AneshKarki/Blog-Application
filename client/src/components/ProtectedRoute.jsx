d60d6ca73cd1f5f1db884bc3d0bf5fcc635554c8
AneshKarki <aneshkarki07@gmail.com>
2025-02-09 21:02:42 +0545

    hahahah

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,10 +1,9 @@
-import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { isLoggedIn } = useContext(AuthContext);
-  console.log(isLoggedIn)
+  // const { isLoggedIn } = useContext(AuthContext);
+  const isLoggedIn = localStorage.getItem("token");
+  console.log(isLoggedIn);
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
 
